a8b4b9da95bfbf7c5553fde31bda2304cf3cca3f
nigolred <nicolo.golinucci@gmail.com>
2022-02-25 11:42:28 +0100

    Adding the needed plots
    Now the main plots are set. Still groupby issue when trying to print the
    plots for the whole dataset.

--- workspace.py
+++ workspace.py
@@ -7,20 +7,48 @@ import pandas as pd
 db_path = pd.read_excel(r'Database/path.xlsx', index_col=0).loc['MIT_Parco_Circolante','Path']
 
 #%%
+sn = slice(None)
 st = time()
 data = DataManager(path=db_path, main_file=r'Database/Regioni_parco_circolante.xlsx')        
-d = data.parse(years=2019, regions=['Abruzzo'])
+data.parse(years=2019)
 nd = time()
 print(f'timing: {nd-st}')
 
 ind = data.Data.columns
+#%%
 
+db = data.Data.loc[(sn,"A",sn,sn,sn,'2019')]
+
+#%%
+import plotly.express as px
+
+fig = px.scatter(db.reset_index(), x='kW', y='massa complessiva', color='segment')
+fig.write_html('Plots\segmentation.html')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#%%
 chart = data.Data.loc[(slice(None),slice(None),slice(None),slice(None),slice(None),"2019"),'count'].groupby(['power train','marca'], axis=0).sum().unstack().fillna(0)
 chart_2 = data.Data.loc[(slice(None),"A",slice(None),"PROPRIO",slice(None),"2019"),('Well to Tank','Tank to Wheel','Carbon Footprint - Conventional Material')].dropna(subset=['Carbon Footprint - Conventional Material']).groupby(['region','power train']).mean().fillna(0)
 chart_2L = data.Data.loc[(slice(None),"A",slice(None),"PROPRIO",slice(None),"2019"),('Well to Tank','Tank to Wheel','Carbon Footprint - Lightweight Material')].dropna(subset=['Carbon Footprint - Lightweight Material']).groupby(['region','power train']).mean().fillna(0)
 
 
 chart_3 = chart_2 * [100000/1000,100000/1000,1/1000]
+#%%
+data.Data['segment'].value_counts() 
+
 #%%
 regions = list(set(chart_3.index.get_level_values(0)))
 col = ['Saved emissions per km - EV vs ICEV [g/km]','km to emission parity [km] - Conventional','km to emission parity [km] - Lightweight']
